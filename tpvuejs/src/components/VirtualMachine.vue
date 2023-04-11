62da1f7ebad532089e9dc20652e2d7618fde8c33
ToniPortal <tonitoctoc13@gmail.com>
2023-04-11 03:12:24 +0200

    envoie

--- tpvuejs/src/components/VirtualMachine.vue
+++ tpvuejs/src/components/VirtualMachine.vue
@@ -1,83 +1,82 @@
 <template>
-  <div class="card-body">
-    <div class="card-columns">
-      <div class="card bg-light">
-        <div class="card-header">
-          <div class="form-inline">
-            <label>VM {{ msg }}</label>
-            <button class="btn x-square-fill ml-auto" type="button"  @click="remove()">X</button>
+  <div class="card bg-light">
+    <div class="card-header">
+      <div class="form-inline">
+        <label>VM {{ msg }}</label>
+        <button
+          class="btn x-square-fill ml-auto"
+          type="button"
+          @click="remover()"
+        >
+          X
+        </button>
+      </div>
+    </div>
+    <div class="card-body">
+      <div class="form-row">
+        <label for="inputEmail3">New Parameter</label>
+        <select class="form-control" id="exampleFormControlSelect1">
+          <option disabled>Mémoire</option>
+          <option>Taille HDD</option>
+          <option>Coeur</option>
+          <option>Socket</option>
+        </select>
+      </div>
+      <div class="form-group">
+        <label for="inputName">Mémoire</label>
+        <div class="input-group mb-3">
+          <input
+            type="text"
+            class="form-control"
+            placeholder=""
+            aria-label="Exemple de texte avec un bouton Addon"
+            aria-describedby="button-addon1"
+            v-model="vmemoire"
+          />
+          <div class="input-group-prepend">
+            <button class="btn trash-fill" type="button"></button>
           </div>
         </div>
-        <div class="card-body">
-          <div class="form-row">
-            <label for="inputEmail3">New Parameter</label>
-            <select class="form-control" id="exampleFormControlSelect1">
-              <option disabled>Mémoire</option>
-              <option>Taille HDD</option>
-              <option>Coeur</option>
-              <option>Socket</option>
-            </select>
+
+        <label for="inputName">HDD</label>
+        <div class="input-group mb-3">
+          <input
+            type="text"
+            class="form-control"
+            placeholder=""
+            aria-label="Exemple de texte avec un bouton Addon"
+            aria-describedby="button-addon1"
+          />
+          <div class="input-group-prepend">
+            <button class="btn trash-fill" type="button"></button>
           </div>
-          <div class="form-group">
-            <label for="inputName">Mémoire</label>
-            <div class="input-group mb-3">
-              <input
-                type="text"
-                class="form-control"
-                placeholder=""
-                aria-label="Exemple de texte avec un bouton Addon"
-                aria-describedby="button-addon1"
-                v-model="vmemoire"
-              />
-              <div class="input-group-prepend">
-                <button class="btn trash-fill" type="button"></button>
-              </div>
-            </div>
+        </div>
 
-            <label for="inputName">HDD</label>
-            <div class="input-group mb-3">
-              <input
-                type="text"
-                class="form-control"
-                placeholder=""
-                aria-label="Exemple de texte avec un bouton Addon"
-                aria-describedby="button-addon1"
-                
-              />
-              <div class="input-group-prepend">
-                <button class="btn trash-fill" type="button"></button>
-              </div>
-            </div>
+        <label for="inputName">Coeur</label>
+        <div class="input-group mb-3">
+          <input
+            type="text"
+            class="form-control"
+            placeholder=""
+            aria-label="Exemple de texte avec un bouton Addon"
+            aria-describedby="button-addon1"
+          />
+          <div class="input-group-prepend">
+            <button class="btn trash-fill" type="button"></button>
+          </div>
+        </div>
 
-            <label for="inputName">Coeur</label>
-            <div class="input-group mb-3">
-              <input
-                type="text"
-                class="form-control"
-                placeholder=""
-                aria-label="Exemple de texte avec un bouton Addon"
-                aria-describedby="button-addon1"
-               
-              />
-              <div class="input-group-prepend">
-                <button class="btn trash-fill" type="button"></button>
-              </div>
-            </div>
-
-            <label for="inputName">Socket</label>
-            <div class="input-group mb-3">
-              <input
-                type="text"
-                class="form-control"
-                placeholder=""
-                aria-label="Exemple de texte avec un bouton Addon"
-                aria-describedby="button-addon1"
-               
-              />
-              <div class="input-group-prepend">
-                <button class="btn trash-fill" type="button"></button>
-              </div>
-            </div>
+        <label for="inputName">Socket</label>
+        <div class="input-group mb-3">
+          <input
+            type="text"
+            class="form-control"
+            placeholder=""
+            aria-label="Exemple de texte avec un bouton Addon"
+            aria-describedby="button-addon1"
+          />
+          <div class="input-group-prepend">
+            <button class="btn trash-fill" type="button"></button>
           </div>
         </div>
       </div>
@@ -96,35 +95,46 @@ export default {
   data() {
     return {
       store: datas,
-      vmemoire: 0
+      vmemoire: 0,
     };
   },
   mounted() {
-    console.log("Meire: " + this.vmemoire)
-    //this.memoire = this.store.liste[this.msg].memoire;
-    //console.log("Store mem: " + this.store.liste[this.msg]["memoire"])
-    if(this.store.liste[this.msg] != undefined){
-
-    console.log("Store Mem = ",this.store.liste[this.msg].id)
-
+    if (this.store.liste[this.msg] != undefined) {
+      console.log("[Monted] ; DATA = ", this.store.liste[this.msg]);
+      console.log("[Monted] ; Store id = ", this.store.liste[this.msg].id);
+      console.log(
+        "[Monted] ; memoire data = ",
+        this.store.liste[this.msg].memoire
+      );
+      this.vmemoire = this.store.liste[this.msg].memoire;
     }
   },
   watch: {
-    changeinfo() {
+    vmemoire() {
+      // console.log("[Watch] ; Vmemoire", this.vmemoire);
 
-      this.store.liste[this.msg].memoire = this.memoire;
+      this.store.liste[this.msg].memoire = this.vmemoire;
 
+      console.log("[Watch] ; Enregistre:", this.store.liste[this.msg].memoire);
     },
   },
-  /*
   methods: {
-    remove() {
-      
-      console.log("remove" + this.nbvm.number)
-      this.nbvm.number--;
-      
-    }
-  }*/
+    remover() {
+      console.log("[Methods] ; Remove", this.msg);
 
+      for (var i = 0; i < this.store.liste.length; i++) {
+        console.log(this.store.liste[i]);
+        console.log(this.msg);
+        if (this.store.liste[i].id === this.msg) {
+          console.log("Remove For");
+          this.store.liste.splice(i, 1);
+          this.store.number--;
+        }
+      }
+
+      console.log("[Methods] StoreafeterRemove :", this.store);
+      
+    },
+  },
 };
 </script>
